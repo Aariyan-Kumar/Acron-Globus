36f293ebc7dbf7fd91a654fc01bc98e755e912e1
Aariyan-Kumar <cst.21bctc85@silicon.ac.in>
2024-11-16 14:09:38 +0530

    first commit

---
+++ stackoverflow-clone/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-3xl text-blue-700"> hello world </h1>
+    </>
+  )
+}
+
+export default App

04800b508ca18b2a1cd3756eb1071ef12a4ebb59
Aariyan-Kumar <cst.21bctc85@silicon.ac.in>
2024-11-17 23:03:50 +0530

    Css Changed : Added cursor pointer and width increaased, border removed

--- stackoverflow-clone/src/components/Sidebar.jsx
+++ stackoverflow-clone/src/components/Sidebar.jsx
@@ -51,7 +51,7 @@ function Sidebar() {
 
     return (
         <>
-            <div className='max-w-64 min-h-screen'>
+            <div className='border-2 border-transparent max-w-72 min-h-screen'>
                 <div className='max-w-[15rem] px-2 mx-7 my-10'>
                     <Link to="/">
                         <img src={SideLogo} alt="stackoverflow side logo" />
@@ -61,18 +61,18 @@ function Sidebar() {
                 <div>
                     <ul className='bg-[#fef4ec] text-justify py-3 px-10'>
                         <div className='flex'>
-                            <img className='w-5 mr-3 py-1' src={HomeIcon} alt="home icon" />
-                            <span className='font-[550]'>Home</span>
+                            <img className='cursor-pointer w-5 mr-3 py-1' src={HomeIcon} alt="home icon" />
+                            <span className='cursor-pointer font-[550]'>Home</span>
                         </div>
                     </ul>
                     <ul className='text-justify pt-5 px-10'>
                         <div className='flex'>
-                            <img className='w-5 mr-3 py-1' src={PublicIcon} alt="public icon" />
-                            <span className='text-gray-500 text-sm my-1 font-semibold'>PUBLIC</span>
+                            <img className='w-5 cursor-pointer mr-3 py-1' src={PublicIcon} alt="public icon" />
+                            <span className='text-gray-500 text-sm my-1 cursor-pointer font-semibold'>PUBLIC</span>
                         </div>
                         {publicItems.map((item) => (
                             <li key={item.name}
-                                className='font-semibold ml-8 py-[0.3rem] text-gray-600'>
+                                className='font-semibold cursor-pointer ml-8 py-[0.3rem] text-gray-600'>
                                 <NavLink
                                     to={item.url}
                                 >
@@ -83,12 +83,12 @@ function Sidebar() {
                     </ul>
                     <ul className='text-justify pt-5 px-10'>
                         <div className='flex'>
-                            <img className='w-5 mr-3 py-1' src={CollectiveIcon} alt="Collective icon" />
+                            <img className='w-5 mr-3 cursor-pointer py-1' src={CollectiveIcon} alt="Collective icon" />
                             <span className='text-gray-500 text-sm my-1 font-semibold'>COLLECTIVES</span>
                         </div>
                         {collectivesItems.map((item) => (
                             <li key={item.name}
-                                className='font-semibold ml-8 py-[0.3rem] text-gray-600'
+                                className='font-semibold cursor-pointer ml-8 py-[0.3rem] text-gray-600'
                             >
                                 <NavLink
                                     to={item.url}
@@ -100,12 +100,12 @@ function Sidebar() {
                     </ul>
                     <ul className='text-justify pt-5 px-10'>
                         <div className='flex'>
-                            <img className='w-5 mr-3 py-1' src={JobIcon} alt="job icon" />
-                            <span className='text-gray-500 text-sm my-1 font-semibold'>FIND JOBS</span>
+                            <img className='w-5 mr-3 py-1 cursor-pointer' src={JobIcon} alt="job icon" />
+                            <span className='text-gray-500 cursor-pointer text-sm my-1 font-semibold'>FIND JOBS</span>
                         </div>
                         {findJobsItems.map((item) => (
                             <li key={item.name}
-                                className='font-semibold ml-8 py-[0.3rem] text-gray-600'
+                                className='font-semibold cursor-pointer ml-8 py-[0.3rem] text-gray-600'
                             >
                                 <NavLink
                                     to={item.url}
@@ -117,13 +117,13 @@ function Sidebar() {
                     </ul>
                     <ul className='text-justify pt-5 px-10'>
                         <div className='flex'>
-                            <img className='w-5 mr-3 py-1' src={TeamsIcon} alt="Teams icon" />
-                            <span className='text-gray-500 text-sm my-1 font-semibold'>TEAMS</span>
+                            <img className='w-5 mr-3 py-1 cursor-pointer' src={TeamsIcon} alt="Teams icon" />
+                            <span className='text-gray-500 cursor-pointer text-sm my-1 font-semibold'>TEAMS</span>
                         </div>
                         {teamsItems.map((item) => (
                             <li key={item.name}
-                                className='font-semibold ml-8 py-[0.3rem] text-gray-600'
-                            >                            
+                                className='font-semibold cursor-pointer ml-8 py-[0.3rem] text-gray-600'
+                            >
                                 <NavLink
                                     to={item.url}
                                 >

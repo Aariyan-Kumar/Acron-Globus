c326884bef2f857c4e2be501baf707e77c7cbed8
Aariyan-Kumar <cst.21bctc85@silicon.ac.in>
2024-11-19 13:53:16 +0530

    index.js pages created for importing all the Pages and expoting from one file

---
+++ stackoverflow-clone/src/pages/index.jsx
@@ -0,0 +1,13 @@
+import Home from "./Home";
+import Bountied from "./Bountied";
+import Hot from "./Hot";
+import Week from "./Week";
+import Month from "./Month"
+
+export {
+    Home,
+    Bountied,
+    Hot,
+    Week,
+    Month
+}

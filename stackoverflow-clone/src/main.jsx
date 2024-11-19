52cb081afd0636106b17ca32aeb984b8725f96e9
Aariyan-Kumar <cst.21bctc85@silicon.ac.in>
2024-11-19 13:44:12 +0530

    Added new Routes within the Home Route and assign the path and the element for new route

--- stackoverflow-clone/src/main.jsx
+++ stackoverflow-clone/src/main.jsx
@@ -3,18 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import { Home, Bountied, Week, Hot, Month } from './pages/index.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      <Route path='/' element={<Home />} />      
+      <Route path='/' element={<Home />}>
+        <Route path='/bountied' element={<Bountied />} />
+        <Route path='/hot' element={<Hot />} />
+        <Route path='/week' element={<Week />} />
+        <Route path='/month' element={<Month />} />
+      </Route>
     </Route>
   )
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )

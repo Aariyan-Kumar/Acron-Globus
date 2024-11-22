5b3682829f078bd5df015ea9f09583a5809289a5
Aariyan-Kumar <cst.21bctc85@silicon.ac.in>
2024-11-22 14:28:13 +0530

    Search component was added to the index.js of Components

--- stackoverflow-clone/src/pages/index.js
+++ stackoverflow-clone/src/pages/index.js
@@ -4,6 +4,7 @@ import Hot from "./Hot";
 import Week from "./Week";
 import Month from "./Month";
 import Interesting from "./Interesting";
+import Search from "./Search";
 
 export {
     Home,
@@ -11,5 +12,6 @@ export {
     Hot,
     Week,
     Month,
-    Interesting
+    Interesting,
+    Search
 }
